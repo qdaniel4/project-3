9360195a5920dd13bc36573ee3999ec975378bb2
qdaniel4 <su0978hi@go.minnstate.edu>
2021-09-29 19:30:47 -0500

    added some functions for workflow ideas

---
+++ db_manager.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('quiz_data_questions_db.db')
+
+def add_user():
+    #todo add users
+
+def get_topic_by_id():
+    #todo get topic
+
+def get_questions():
+    #todo get questions
+
+def get_choices():
+    #todo get multiple choices for questions
+
+def get_points_for_question():
+    #todo get question points
+
+def get_difficulty():
+    #todo get question difficulty
+
+def get_session_info():
+    #todo get session info
